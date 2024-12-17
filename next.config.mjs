2b4f398b51d36b82649e337df22e58711fcf78d7
T31K <t31kmunwong@gmail.com>
2024-12-17 16:31:06 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["hackalumpur.com"], // Add your image domain(s) here
+  },
+};
 
 export default nextConfig;
